962b50d3d3e4917ac7248a1f6376215e5763a9d0
Nicolás Proto <nicolasproto@gmail.com>
2019-05-13 19:46:47 -0300

    Click events

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-  $('#snow')
-    .parent() // Parent to go up, children to go down
-    .find('.title')
-    .addClass('highlight');
-
-    console.log($('#snow').closest('.temperature'));
+  $('.box').on('click', function(){
+    // 'this' calls the object that called the function
+    // 'toggleClass' sets the class or removes it if it already has it
+    $(this).toggleClass('highlight');
+  })
 });
 
 
-
-
 // .parent() to go up, .children() to go down
+
+// $('#snow')
+//   .parent() // Parent to go up, children to go down
+//   .find('.title')
+//   .addClass('highlight');
