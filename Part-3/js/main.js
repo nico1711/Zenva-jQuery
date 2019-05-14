cc9daddc307f8bfe196967adfeba75b9b089794b
Nicolás Proto <nicolasproto@gmail.com>
2019-05-14 17:40:35 -0300

    Removing elements

--- Part-3/js/main.js
+++ Part-3/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+  // Adding an element with the Add button
   $('#button-create-item').on('click', function() {
     // Gets the value in a variable
     let name = $('#input-create-item').val();
     // Cleans the input bar
     $('#input-create-item').val('');
 
+    // Adds the name to the html template
     let html = '';
     html += '<div class="item">'
       html += '<div class="name">' + name + '</div>'
@@ -15,12 +17,17 @@ $(document).ready(function(){
         html += 'aliqua.'
       html += '</div>'
       html += '<div class="price">499</div>'
-      html += '<button>Add to cart</button><br>'
+      html += '<button class="item-add">Add to cart</button>'
+      html += '<button class="item-remove">Remove</button><br>'
       html += '<a href="#">More Info</a>'
       html += '<div class="more info">Lorem ipsum dolor sit amet.</div>'
     html += '</div>'
 
     $('#container').prepend(html);
+  });
 
+  // Removing an element with the remove button
+  $('#container').on('click', '.item-remove', function() {
+    $(this).parent().remove();
   });
 });
