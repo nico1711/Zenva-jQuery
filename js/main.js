3acd31aafad8c2e1f0a506ba6468e10bf82e776e
Nicolás Proto <nicolasproto@gmail.com>
2019-05-14 13:14:35 -0300

    Change event

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-  $('.box').on('click','.box-button', function(){
-    // 'this' calls the object that called the function
-    // 'toggleClass' sets the class or removes it if it already has it
-    $(this).parent().toggleClass('highlight');
-    console.log("hello");
-  })
+  $('#select-menu').on('change', function() {
+    let name = $('#select-menu option:selected').text();
+    let distance = $('#select-menu option:selected').val();
+    // If you set a data-price attribute, you call it with data('price')
+    let price = $('#select-menu option:selected').data('price');
+
+    console.log(price);
+    $('#feedback-message')
+    .text('You are signing up for a ' + name +
+    ', wich costs ' + price +
+    ', to a distance of ' + distance + 'km');
+  });
 });
 
 
@@ -14,3 +20,9 @@ $(document).ready(function(){
 //   .parent() // Parent to go up, children to go down
 //   .find('.title')
 //   .addClass('highlight');
+
+// $('.box').on('click','.box-button', function(){
+  // 'this' calls the object that called the function
+  // 'toggleClass' sets the class or removes it if it already has it
+  // $(this).parent().toggleClass('highlight');
+// })
